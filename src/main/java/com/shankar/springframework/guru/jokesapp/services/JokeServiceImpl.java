ffc6fdbebb7575608b7625db02a2d10a5cace17a
shankar.pachari <shankar.pachari@gmail.com>
2020-10-04 13:44:09 -0700

    fixed Jokes service constructor

--- src/main/java/com/shankar/springframework/guru/jokesapp/services/JokeServiceImpl.java
+++ src/main/java/com/shankar/springframework/guru/jokesapp/services/JokeServiceImpl.java
@@ -1,15 +1,17 @@
 package com.shankar.springframework.guru.jokesapp.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 
 @Service
 public class JokeServiceImpl implements JokeService {
 
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
-        this.chuckNorrisQuotes = chuckNorrisQuotes;
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
     }
 
     @Override
